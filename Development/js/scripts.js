243e4212fb481669b5d4952f4cf9b543c113a182
malakalhujailan <66518892+malakalhujailan@users.noreply.github.com>
2020-07-02 22:25:05 +0300

    done

---
+++ Development/js/scripts.js
@@ -0,0 +1,70 @@
+// التمرين الأول الجزئية الأولى //
+booksname = ["شجرتي شجرة البرتقال", "الفقراء", "حالات نادرة" , "فئران أمي حصة"]
+booksname.forEach(logger)
+function logger(value){
+    console.log(value)
+}
+
+// التمرين الأول الجزئية الثانية //
+let hotterdays  = [
+    50, 51, 47 , 40 
+]
+function myfunction (primary) {
+ return i > 60
+}
+
+
+let books = [{
+  name: "كوخ العم توم",
+  ID:1,
+},
+{
+    name: "الجريمة و العقاب",
+    ID:2,
+  },
+  {
+    name: "ثم اختفى كل شيء",
+    ID:3,
+  }
+];
+function getbookbyId (books,ID)
+{
+    for (x of books) {
+        if(x.ID==ID);
+        return 1;
+    }
+}
+
+
+
+
+
+
+
+
+// التمرين الثاني //
+let done = [];
+let cart =[];
+while (done != "انتهيت"){
+let end = prompt("ادخل انتهيت عند الإنتهاء من التسوق")
+{
+    let items= parseInt(prompt ("السلعة"))
+    let prices= parsefloat(prompt ("السعر"))
+   
+    
+    cart.push(items);
+    cart.push(prices)
+}
+if (done ="انتهيت")
+console.log(items);
+console.log(prices);
+}
+function print(items)
+function print(prices)
+{
+    let total = 0 ;
+    console.log("الفاتورة")
+    console.log(items)
+    console.log(prices)
+    console.log("thank you, shop again soon")
+}
